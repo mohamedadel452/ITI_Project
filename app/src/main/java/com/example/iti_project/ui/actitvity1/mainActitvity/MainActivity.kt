defaabd111f65cb36bad6e7a2d2ee17c0e045105
mohamedadel452 <m7711843@gmail.com>
2024-08-18 12:31:37 +0300

    mohamed adel add ignore file

--- app/src/main/java/com/example/iti_project/ui/actitvity1/mainActitvity/MainActivity.kt
+++ app/src/main/java/com/example/iti_project/ui/actitvity1/mainActitvity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.iti_project.ui.actitvity1.mainActitvity
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,10 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+
+
+
+
+}
+
 }
