d54cc1679a01ca2208ff6b17b109adeb22c7dc60
mohamedadel452 <m7711843@gmail.com>
2024-08-22 15:31:20 +0300

    mohamed adel start for details

---
+++ app/src/main/java/com/example/iti_project/ui/RecipeActivity/DetailsFragment/DetailsFragment.kt
@@ -0,0 +1,30 @@
+package com.example.iti_project.ui.RecipeActivity.DetailsFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.iti_project.R
+
+
+class DetailsFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+
+
+}
