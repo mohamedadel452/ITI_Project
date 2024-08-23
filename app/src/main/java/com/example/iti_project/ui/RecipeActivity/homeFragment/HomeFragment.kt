f6e8592d5c6eb2dc87b7525a9522c784d616c24f
NadaAlsaid <nadagaber619@gmail.com>
2024-08-23 13:40:57 +0300

    update feature favourite in SearchFragment

---
+++ app/src/main/java/com/example/iti_project/ui/RecipeActivity/homeFragment/HomeFragment.kt
@@ -0,0 +1,142 @@
+package com.example.iti_project.ui.RecipeActivity.homeFragment
+
+import android.app.ProgressDialog
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.appcompat.widget.SearchView
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.iti_project.R
+import com.example.iti_project.data.DataSource.LocalDataSource.InterFace.LocalDataSourceImp
+import com.example.iti_project.data.DataSource.LocalDataSource.LocalData.RoomDatabase.RoomDataBaseImp
+import com.example.iti_project.data.DataSource.LocalDataSource.LocalData.SharedPrefrence.SharedPreferenceImp
+import com.example.iti_project.data.models.UiState
+import com.example.iti_project.data.repo.Meals.MealsRepoImpl
+import com.example.iti_project.data.repo.favouriteRepo.FavoriteRecipeRepoImp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var mProgressDialog: ProgressDialog
+    private val viewModel: HomeFragmentViewModel by viewModels() {
+        ProductViewModelFactory(
+            MealsRepoImpl(), FavoriteRecipeRepoImp(
+                LocalDataSourceImp(
+                    requireContext(),
+                    RoomDataBaseImp.getInstance(requireContext()),
+                    SharedPreferenceImp.getInstance(requireContext())
+                )
+            )
+        )
+    }
+    private lateinit var adapter: AdapterForListRecipe
+    private lateinit var searchView: ImageButton
+    override fun onStart() {
+        super.onStart()
+        viewModel.getMeals()
+        viewModel.getFavoriteList()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = view.findViewById(R.id.list_recipe)
+        mProgressDialog = ProgressDialog(requireContext())
+        adapter = AdapterForListRecipe{
+            val action = HomeFragmentDirections.actionHomeToRecipeDetailsFragment(it)
+            findNavController().navigate(action)
+        }
+        searchView = view.findViewById(R.id.imageButton)
+
+        searchView.setOnClickListener{
+            findNavController().navigate(R.id.search)
+        }
+
+
+
+        GlobalScope.launch(Dispatchers.Main) {
+            recyclerView.adapter = adapter
+            recyclerView.layoutManager =
+                StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+
+        }
+
+        viewModel.meals.observe(viewLifecycleOwner) { meals ->
+            when (meals) {
+                is UiState.Error -> {
+                    mProgressDialog.cancel()
+                    Toast.makeText(
+                        requireContext(),
+                        "there is an error :${meals.errorMessage}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.e("error", meals.errorMessage)
+                }
+
+                is UiState.Loading -> {
+                    mProgressDialog.setTitle("Loading Data")
+                    mProgressDialog.setMessage("please wait while we are loading data")
+                    mProgressDialog.show()
+                    Toast.makeText(requireContext(), "loading ", Toast.LENGTH_SHORT).show()
+                }
+
+                is UiState.Success -> {
+                    GlobalScope.launch(Dispatchers.Main) {
+                        adapter.setData(meals.data , viewModel.favoriteUserIds)
+                    }
+
+                    mProgressDialog.cancel()
+                }
+
+            }
+        }
+
+        listenToDeleteFavouriteItems()
+        listenToAddFavouriteItems()
+    }
+
+
+    private fun listenToDeleteFavouriteItems() {
+        adapter.favoriteUserRemovedIds.observe(viewLifecycleOwner) { response ->
+            if (!response.isNullOrEmpty() ) {
+                viewModel.deleteFavoriteRecipe(response)
+                adapter.updateIDs()
+//                favoriteRecipesAdapter.setData(viewModel.favoriteRecipes , viewModel.favoriteUserIds)
+            }
+        }
+    }
+
+    private fun listenToAddFavouriteItems() {
+        adapter.favoriteUserAddMeal.observe(viewLifecycleOwner) { response ->
+            if (response != null ) {
+                viewModel.addFavoriteRecipe(response)
+                adapter.updateIDs()
+//                favoriteRecipesAdapter.setData(viewModel.favoriteRecipes , viewModel.favoriteUserIds)
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewModel.getFavoriteList()
+    }
+}
---
+++ app/src/main/java/com/example/iti_project/ui/RecipeActivity/homeFragment/HomeFragmentViewModel.kt
@@ -0,0 +1,114 @@
+package com.example.iti_project.ui.RecipeActivity.homeFragment
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.iti_project.data.models.Meals
+import com.example.iti_project.data.models.ResultState
+import com.example.iti_project.data.models.UiState
+import com.example.iti_project.data.repo.Meals.MealsRepo
+import com.example.iti_project.data.repo.favouriteRepo.FavoriteRecipeRepo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class HomeFragmentViewModel(private val repository: MealsRepo , private val  favoriteRecipeRepo: FavoriteRecipeRepo ) : ViewModel() {
+
+    private val _meals: MutableLiveData<UiState<List<Meals>>> = MutableLiveData(UiState.Loading)
+    val meals: LiveData<UiState<List<Meals>>> = _meals
+
+
+    private val _isSaved = MutableLiveData<Boolean>(false)
+
+
+    fun getMeals() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+
+                val response = async {  repository.getMeals()}.await()
+
+                // Switch to Main thread for UI updates
+                when (response) {
+                    is ResultState.Success -> {
+                        _meals.postValue(UiState.Success(response.data.meals))
+                    }
+                    is ResultState.Error -> {
+                        _meals.postValue(UiState.Error(response.errorMessage))
+                    }
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Log.e("HomeFragmentViewModel", "Error fetching meals", e)
+                    _meals.postValue(UiState.Error(e.toString()))
+                }
+            }
+        }
+    }
+
+    var favoriteRecipes : MutableList<Meals> = mutableListOf()
+    var favoriteUserIds:  MutableList<String> = mutableListOf()
+    init {
+
+        getFavoriteList()
+
+    }
+    fun addFavorite(id: String){
+        favoriteUserIds.add(id)
+    }
+
+    fun removeFavorite(id: String){
+        favoriteUserIds.remove(id)
+
+    }
+
+    fun addFavoriteRecipe(meal: Meals){
+        if (meal != null) {
+
+            GlobalScope.launch(Dispatchers.IO) {
+                favoriteRecipeRepo.addFavouriteRecipe(meal)
+            }
+            addFavorite(meal.idMeal)
+        }
+    }
+
+    fun deleteFavoriteRecipe(id: String){
+        if (id != null) {
+
+            GlobalScope.launch(Dispatchers.IO) {
+                favoriteRecipeRepo.deleteFavouriteRecipeList(id)
+
+            }
+            removeFavorite(id)
+        }
+    }
+
+    fun  getFavoriteList(){
+        GlobalScope.launch(Dispatchers.IO) {
+
+            favoriteRecipeRepo.getRecipes()
+            delay(200)
+            favoriteRecipes = favoriteRecipeRepo.favoriteRecipe.toMutableList()
+            favoriteUserIds = favoriteRecipeRepo.favoriteRecipeIDs.toMutableList()
+            Log.i("favoriteRecipes", ""+favoriteRecipes.size)
+        }
+    }
+
+}
+
+class ProductViewModelFactory(private val repository: MealsRepo , private val  favoriteRecipeRepo: FavoriteRecipeRepo) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HomeFragmentViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return HomeFragmentViewModel(repository , favoriteRecipeRepo) as T
+        } else {
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
+}
